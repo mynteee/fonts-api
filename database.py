0f9a61f7522e7167372b45d3de30d6b7e7533036
Matthew Ji <jimatthew.69@gmail.com>
2025-02-20 19:03:24 -0800

    i added fonts
    identifying fonts from old projects is so hard

--- database.py
+++ database.py
@@ -2,8 +2,8 @@
 entry explanations
 
 {
-"id": 0,                                the "id" of the font; for this case is is in the sequential order that i decided to add them
-"name": "sample font",                  the name of the font in css
+"id": 0,                                the "id" of the font; for this case it is in the sequential order that i decided to add them
+"name": "sample font",                  the name of the font in css (if not applicable use googleapis; if still not applicable use file name)
 "family": "sample family",              one of the 5 generic font families in css (serif, sans-serif, monospace, cursive, fantasy)
 "projects": ["project1", "project2"],   list of the projects ive used the font in
 "serif": True                           if the font is serif (this is only sometimes redundant)
@@ -11,5 +11,8 @@ entry explanations
 '''
 
 data = [
-    {"id": 0, "name": "sample font", "family": "sample family", "projects": ["project1", "project2", "project3"], "serif": True}, #sample entry
+    {"id": 0, "name": "sample font",    "family": "sample family",  "projects": ["project1", "project2", "project3"],                           "serif": None}, # sample entry
+    {"id": 1, "name": "Bebas Neue",     "family": "sans-serif",     "projects": ["da-poster"],                                                  "serif": False},
+    {"id": 2, "name": "Magilio",        "family": "serif",          "projects": ["cards", "check-please-program", "check-please-shirt"],        "serif": True},
+    {"id": 3, "name": "Copperplate",    "family": "serif",          "projects": ["da-program"],                                                 "serif": True},
 ]
